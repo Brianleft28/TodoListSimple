c2541883cba1dba0026ce56efa034d60def09040
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-06-01 12:29:03 -0300

    logica de tareas y responsive taskboard

--- src/components/TaskBoard.svelte
+++ src/components/TaskBoard.svelte
@@ -1,11 +1,25 @@
 <script>
+  import { onMount } from "svelte";
   import TaskForm from "./TaskForm.svelte";
   let title = '';
   let description = '';
   let tasks = [];
 
+  // Cargar las tareas del localstorage
+  onMount(() => {
+    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  });
+
+  // funcion para agregar tareas
   function handleSubmit(e) {
     e.preventDefault();
+    
+    // condiciones para validar los campos
+    if (!title || !description) {
+      alert('Todos los campos son requeridos');
+      return;
+    } 
+    
     /* Crear la tarea, y el array de tareas */
     const task = {
       id: tasks.length + 1,
@@ -21,16 +35,41 @@
 
      /* Guardar el localstorage  */
 
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
 
+  // funcion para eliminar tareas
+  function handleDelete(id) {
+    const newTasks = tasks.filter(task => task.id !== id);
+    tasks = newTasks;
+    localStorage.setItem('tasks', JSON.stringify(newTasks))
   }
 
 </script>
 
 
-<div class="container mx-auto flex justify-center mt-8"> 
-    <form >
+<div class="container mx-auto flex flex-col justify-center mt-8"> 
     <TaskForm bind:title bind:description  on:submit={handleSubmit}/>
-    </form>
+    <div class="mx-auto px-3 w-full  border-x border-secondary/5 ">
+    <div class="bg-base-300">
+      {#if tasks.length > 0}
+        <ul class="list-none">
+          {#each tasks as task}
+            <li class="flex justify-between p-3 border-b border-secondary/5">
+              <div>
+                <h3 class="text-lg font-bold">{task.title}</h3>
+                <p>{task.description}</p>
+              </div>
+              <button on:click={handleDelete(task.id)} class="btn btn-secondary">Eliminar</button>
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <p class="text-center p-3">No hay tareas</p>
+      {/if}
+    </div>
+  </div>
+
 </div>
 
 
--- src/components/TaskForm.svelte
+++ src/components/TaskForm.svelte
@@ -10,24 +10,25 @@
     }
 </script>
 
-<form on:submit={submitForm} class="form-control border-2 border-secondary/5  p-3 bg-base-100/15">
+<form on:submit={submitForm} class="form-control mx-auto w-full border-2 border-secondary/5  p-3 bg-base-100/15">
   
-    <div class="shadow-sm flex sm:gap-0 gap-3 lg:flex-row md:flex-row sm:flex-row flex-col  ">
+    <div class="shadow-sm flex sm:gap-0 gap-3 lg:flex-row md:flex-row sm:flex-row flex-col justify-between ">
         
         <input 
         bind:value={title} 
         type="text" placeholder="Titulo" 
-        class="input sm:max-w-[150px] w-full input-bordered" 
+        class="input w-full input-bordered lg:max-w-xs md:max-w-xs sm:max-w-xs"
+         
     />
         <input 
         bind:value={description} 
         type="text" 
         placeholder="Descripción" 
-        class="input input-bordered md: lg:min-w-[500px] md:max-w-xs w-full" 
+        class="input input-bordered w-full" 
     />
         <button 
         type="submit" 
-        class="ml-1 btn btn-primary max-w-xs">
+        class="ml-1 btn btn-primary lg:max-w-xs">
         Agregar tarea
         </button>
 
